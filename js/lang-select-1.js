0592f950de7a351e002f661ecafe00b62e6e119a
ahmed <a.oosman.g@gmail.com>
2021-11-20 10:43:14 +0200

    beautify code

--- js/lang-select-1.js
+++ js/lang-select-1.js
@@ -1,16 +1,16 @@
-const mediaQuery = window.matchMedia('(max-width: 900px)');
+const mediaQuery = window.matchMedia("(max-width: 900px)");
 let lang = "ar";
 let arrow_state = "right";
 //navbar
-let btnlang_1 = document.querySelector('.langBtn_1');
-let btnlang_2 = document.querySelector('.langBtn_2');
+let btnlang_1 = document.querySelector(".langBtn_1");
+let btnlang_2 = document.querySelector(".langBtn_2");
 let groupsdroptitles = document.querySelector(".groupstitle");
 let groupsdropticon = document.querySelector(".fa-caret-down");
-let lastli = document.querySelector('.nav-item:last-child');
-let langtit = document.querySelector('.langtit');
-let changepasswordtit = document.querySelector('.change-password');
-let signout = document.querySelector('.out');
-let sidenav_opt =document.querySelectorAll(".opt");
+let lastli = document.querySelector(".nav-item:last-child");
+let langtit = document.querySelector(".langtit");
+let changepasswordtit = document.querySelector(".change-password");
+let signout = document.querySelector(".out");
+let sidenav_opt = document.querySelectorAll(".opt");
 let arrow_1 = document.querySelector(".toggle-icon");
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".bx-menu");
@@ -29,121 +29,121 @@ let sites = document.querySelector(".sites");
 let osool = document.querySelector(".osool-title");
 let reports = document.querySelector(".reports");
 let settings = document.querySelector(".settings");
-let copyrights = document.querySelector('#copyrights');
+let copyrights = document.querySelector("#copyrights");
 
 //listen to event
-btnlang_1.addEventListener("click",function(){
+btnlang_1.addEventListener("click", function() {
   console.log("btn clicked");
-  if(lang == "ar"){
+  if (lang == "ar") {
     English();
-  }else{
+  } else {
     Arabic();
   }
 });
-btnlang_2.addEventListener("click",function(){
+btnlang_2.addEventListener("click", function() {
   console.log("btn clicked");
-  if(lang == "ar"){
+  if (lang == "ar") {
     English();
-  }else{
+  } else {
     Arabic();
   }
 });
-
 
 function English() {
-lang = "en";
-document.body.style.direction="ltr";
-
-groupsdroptitles.innerHTML ="groups";
-groupsdropticon.style.float = "right";
-langtit.innerHTML  = "Language";
-changepasswordtit.innerHTML  = "Change password";
-signout.innerHTML  = "Signout";
-main_title.innerHTML  = "Main page";
-main_title_side.innerHTML  = "Main page";
-no_sites.innerHTML  = "Number of sites";
-groups[0].innerHTML  = "Groups";
-groups[1].innerHTML  = "Groups";
-main_items.innerHTML  = "Main categories";
-sub_catogery.innerHTML  = "Sub categories";
-osool_collections.innerHTML ="Osool collections";
-alahad[0].innerHTML ="Alahd";
-alahad[1].innerHTML ="Alahd";
-users[0].innerHTML ="Users";
-users[1].innerHTML ="Users";
-sites.innerHTML ="Sites";
-osool.innerHTML ="Osool";
-reports.innerHTML ="Reports";
-settings.innerHTML ="Settings";
-copyrights.innerHTML = "all copyrights are reserved.osol@2021";
-lastli.style.marginLeft ="0";
-lastli.style.marginRight ="25px";
-for (var i = 0; i < sidenav_opt.length; i++) {
-  sidenav_opt[i].style.marginRight ="0px";
-  sidenav_opt[i].style.marginLeft ="10px";
-}
-arrow_1.style.float= "right";
-footer.style.background = "linear-gradient(-90deg, rgba(35,168,225,1) 18%, rgba(143,206,233,1) 78%)";
-if (arrow_state == "right") {
-  arrow_1.src = "./img/arrow-left.png";
-  arrow_state = "left";
-}else{
-  arrow_1.src = "./img/arrow-right.png";
-  arrow_state = "right";
-}
+  lang = "en";
+  document.body.style.direction = "ltr";
+  groupsdroptitles.innerHTML = "groups";
+  groupsdropticon.style.float = "right";
+  langtit.innerHTML = "Language";
+  changepasswordtit.innerHTML = "Change password";
+  signout.innerHTML = "Signout";
+  main_title.innerHTML = "Main page";
+  main_title_side.innerHTML = "Main page";
+  no_sites.innerHTML = "Number of sites";
+  groups[0].innerHTML = "Groups";
+  groups[1].innerHTML = "Groups";
+  main_items.innerHTML = "Main categories";
+  sub_catogery.innerHTML = "Sub categories";
+  osool_collections.innerHTML = "Osool collections";
+  alahad[0].innerHTML = "Alahd";
+  alahad[1].innerHTML = "Alahd";
+  users[0].innerHTML = "Users";
+  users[1].innerHTML = "Users";
+  sites.innerHTML = "Sites";
+  osool.innerHTML = "Osool";
+  reports.innerHTML = "Reports";
+  settings.innerHTML = "Settings";
+  copyrights.innerHTML = "all copyrights are reserved.osol@2021";
+  lastli.style.marginLeft = "0";
+  lastli.style.marginRight = "25px";
+  for (var i = 0; i < sidenav_opt.length; i++) {
+    sidenav_opt[i].style.marginRight = "0px";
+    sidenav_opt[i].style.marginLeft = "10px";
+  }
+  arrow_1.style.float = "right";
+  footer.style.background =
+    "linear-gradient(-90deg, rgba(35,168,225,1) 18%, rgba(143,206,233,1) 78%)";
+  if (arrow_state == "right") {
+    arrow_1.src = "./img/arrow-left.png";
+    arrow_state = "left";
+  } else {
+    arrow_1.src = "./img/arrow-right.png";
+    arrow_state = "right";
+  }
 }
 /////////////////////////////////////////////////////////////////
 
 function Arabic() {
-lang = "ar";
-document.body.style.direction="rtl";
+  lang = "ar";
+  document.body.style.direction = "rtl";
 
-groupsdroptitles.innerHTML ="المجموعة";
-groupsdropticon.style.float = "left";
-langtit.innerHTML  = "اللغة";
-changepasswordtit.innerHTML  = "تغيير كلمة السر";
-signout.innerHTML  = "الخروج عن النظام";
-main_title.innerHTML  = "الصفحة الرئيسية";
-main_title_side.innerHTML  = " الصفحة الرئيسية";
-no_sites.innerHTML  = "عدد المواقع";
-groups[0].innerHTML  = "المجموعات";
-groups[1].innerHTML  = "المجموعات";
-main_items.innerHTML  = "الاصناف الرئيسية";
-sub_catogery.innerHTML  = "الاصناف الفرعية";
-osool_collections.innerHTML ="مجموع الاصول";
-alahad[0].innerHTML ="العهد";
-alahad[1].innerHTML ="العهد";
-users[0].innerHTML ="المستخدمين";
-users[1].innerHTML ="المستخدمين";
-sites.innerHTML ="المواقع";
-osool.innerHTML ="الاصول";
-reports.innerHTML ="التقارير";
-settings.innerHTML ="الاعدادات";
-copyrights.innerHTML = "جميع الحقوق محفوظة.اصول@2021";
-lastli.style.marginLeft ="25px";
-lastli.style.marginRight ="0px";
-for (var i = 0; i < sidenav_opt.length; i++) {
-  sidenav_opt[i].style.marginRight ="10px";
-  sidenav_opt[i].style.marginLeft ="0px";
-}
-arrow_1.style.float= "left";
-footer.style.background = "linear-gradient(90deg, rgba(35,168,225,1) 18%, rgba(143,206,233,1) 78%)"
-if (arrow_state == "right") {
-  arrow_1.src = "./img/arrow-left.png";
-  arrow_state = "left";
-}else{
-  arrow_1.src = "./img/arrow-right.png";
-  arrow_state = "right";
-}
+  groupsdroptitles.innerHTML = "المجموعة";
+  groupsdropticon.style.float = "left";
+  langtit.innerHTML = "اللغة";
+  changepasswordtit.innerHTML = "تغيير كلمة السر";
+  signout.innerHTML = "الخروج عن النظام";
+  main_title.innerHTML = "الصفحة الرئيسية";
+  main_title_side.innerHTML = " الصفحة الرئيسية";
+  no_sites.innerHTML = "عدد المواقع";
+  groups[0].innerHTML = "المجموعات";
+  groups[1].innerHTML = "المجموعات";
+  main_items.innerHTML = "الاصناف الرئيسية";
+  sub_catogery.innerHTML = "الاصناف الفرعية";
+  osool_collections.innerHTML = "مجموع الاصول";
+  alahad[0].innerHTML = "العهد";
+  alahad[1].innerHTML = "العهد";
+  users[0].innerHTML = "المستخدمين";
+  users[1].innerHTML = "المستخدمين";
+  sites.innerHTML = "المواقع";
+  osool.innerHTML = "الاصول";
+  reports.innerHTML = "التقارير";
+  settings.innerHTML = "الاعدادات";
+  copyrights.innerHTML = "جميع الحقوق محفوظة.اصول@2021";
+  lastli.style.marginLeft = "25px";
+  lastli.style.marginRight = "0px";
+  for (var i = 0; i < sidenav_opt.length; i++) {
+    sidenav_opt[i].style.marginRight = "10px";
+    sidenav_opt[i].style.marginLeft = "0px";
+  }
+  arrow_1.style.float = "left";
+  footer.style.background =
+    "linear-gradient(90deg, rgba(35,168,225,1) 18%, rgba(143,206,233,1) 78%)";
+  if (arrow_state == "right") {
+    arrow_1.src = "./img/arrow-left.png";
+    arrow_state = "left";
+  } else {
+    arrow_1.src = "./img/arrow-right.png";
+    arrow_state = "right";
+  }
 }
 /////////////////////////
 // sidebar responsive
-sidebarBtn.addEventListener("click", ()=>{
+sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
   if (arrow_state == "right") {
     arrow.src = "./img/arrow-left.png";
     arrow_state = "left";
-  }else{
+  } else {
     arrow.src = "./img/arrow-right.png";
     arrow_state = "right";
   }
--- js/lang-select.js
+++ js/lang-select.js
@@ -1,82 +1,81 @@
-const mediaQuery = window.matchMedia('(max-width: 900px)');
+const mediaQuery = window.matchMedia("(max-width: 900px)");
 let lang = "ar";
 
 // Login page
-let form = document.querySelector('#form');
-let btnlang = document.querySelector('.langBtn');
-let title = document.querySelector('#title');
-let nametitle = document.querySelector('#name-title');
-let passwordtitle = document.querySelector('#password-title');
-let forgetquestion = document.querySelector('#forget-question');
-let reset = document.querySelector('#reset');
-let loginbtn = document.querySelector('.login');
-let copyrightsC = document.querySelector('.copyrights');
-let copyrights = document.querySelector('#copyrights');
-let background = document.querySelector('.login-box');
-let contlogin = document.querySelector('.login-box-content');
+let form = document.querySelector("#form");
+let btnlang = document.querySelector(".langBtn");
+let title = document.querySelector("#title");
+let nametitle = document.querySelector("#name-title");
+let passwordtitle = document.querySelector("#password-title");
+let forgetquestion = document.querySelector("#forget-question");
+let reset = document.querySelector("#reset");
+let loginbtn = document.querySelector(".login");
+let copyrightsC = document.querySelector(".copyrights");
+let copyrights = document.querySelector("#copyrights");
+let background = document.querySelector(".login-box");
+let contlogin = document.querySelector(".login-box-content");
 
 //listen to event from language btn
-btnlang.addEventListener("click",function(){
+btnlang.addEventListener("click", function() {
   console.log("btn clicked");
-  if(lang == "ar"){
+  if (lang == "ar") {
     English();
-  }else{
+  } else {
     Arabic();
   }
 });
 
 // login page the content in english
 function English() {
-lang = "en";
-// Login page
-document.body.style.direction="ltr";
-form.style.direction = "ltr";
-title.innerHTML = "عربي";
-nametitle.innerHTML = "UserName";
-passwordtitle.innerHTML ="password";
-forgetquestion.innerHTML = "Forget password?";
-reset.innerHTML ="Reset";
-loginbtn.innerHTML="Login";
-copyrights.innerHTML = "all copyrights are reserved.osol@2021";
-copyrightsC.style.paddingLeft = "50%";
-copyrightsC.style.paddingRight = "0%";
-background.style.background ="url('./img/home-background-2.png')";
-background.style.backgroundSize ="cover";
-background.style.backgroundPosition ="bottom";
-background.style.height ="100%";
-contlogin.style.marginLeft = "11%";
-if(mediaQuery.matches){
-  background.style.background ="white";
-  copyrightsC.style.paddingLeft = "2%";
+  lang = "en";
+  // Login page
+  document.body.style.direction = "ltr";
+  form.style.direction = "ltr";
+  title.innerHTML = "عربي";
+  nametitle.innerHTML = "UserName";
+  passwordtitle.innerHTML = "password";
+  forgetquestion.innerHTML = "Forget password?";
+  reset.innerHTML = "Reset";
+  loginbtn.innerHTML = "Login";
+  copyrights.innerHTML = "all copyrights are reserved.osol@2021";
+  copyrightsC.style.paddingLeft = "50%";
   copyrightsC.style.paddingRight = "0%";
-  contlogin.style.marginLeft = "0%";
-
-}
+  background.style.background = "url('./img/home-background-2.png')";
+  background.style.backgroundSize = "cover";
+  background.style.backgroundPosition = "bottom";
+  background.style.height = "100%";
+  contlogin.style.marginLeft = "11%";
+  if (mediaQuery.matches) {
+    background.style.background = "white";
+    copyrightsC.style.paddingLeft = "2%";
+    copyrightsC.style.paddingRight = "0%";
+    contlogin.style.marginLeft = "0%";
+  }
 }
 //login page the content in arabic
 function Arabic() {
-lang = "ar";
-// Login page
-document.body.style.direction="rtl";
-form.style.direction = "rtl";
-title.innerHTML = "English";
-nametitle.innerHTML = "اسم المستحدم";
-passwordtitle.innerHTML ="كلمة المرور";
-forgetquestion.innerHTML = "نسيت كلمة السر؟";
-reset.innerHTML= "اعادة تعيين";
-loginbtn.innerHTML="تسجيل الدخول";
-copyrights.innerHTML = "جميع الحقوق محفوظة.اصول@2021";
-copyrightsC.style.paddingLeft = "0%";
-copyrightsC.style.paddingRight = "35%";
-background.style.background ="url('./img/home-background-1.png')";
-background.style.backgroundSize ="cover";
-background.style.backgroundPosition ="bottom";
-
-background.style.height ="100%";
-
-if(mediaQuery.matches){
-  background.style.background ="white";
+  lang = "ar";
+  // Login page
+  document.body.style.direction = "rtl";
+  form.style.direction = "rtl";
+  title.innerHTML = "English";
+  nametitle.innerHTML = "اسم المستحدم";
+  passwordtitle.innerHTML = "كلمة المرور";
+  forgetquestion.innerHTML = "نسيت كلمة السر؟";
+  reset.innerHTML = "اعادة تعيين";
+  loginbtn.innerHTML = "تسجيل الدخول";
+  copyrights.innerHTML = "جميع الحقوق محفوظة.اصول@2021";
   copyrightsC.style.paddingLeft = "0%";
-  copyrightsC.style.paddingRight = "5%";
-}
+  copyrightsC.style.paddingRight = "35%";
+  background.style.background = "url('./img/home-background-1.png')";
+  background.style.backgroundSize = "cover";
+  background.style.backgroundPosition = "bottom";
+
+  background.style.height = "100%";
+
+  if (mediaQuery.matches) {
+    background.style.background = "white";
+    copyrightsC.style.paddingLeft = "0%";
+    copyrightsC.style.paddingRight = "5%";
+  }
 }
